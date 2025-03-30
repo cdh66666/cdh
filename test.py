ab5827a6e6995ae82334c2356ccbe45b1ec1cf6e
cdh <87925267+cdh66666@users.noreply.github.com>
2025-03-30 10:58:35 +0800

    完善整个代码框架

---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+from numpy.random import choice
+from isaacgym import gymutil, gymapi
+# 假设 terrain_utils 模块中包含创建地形的相关辅助函数
+from isaacgym import terrain_utils
+from isaacgym.terrain_utils import SubTerrain
+ 
